cdbcaa41645f27bcfe86687b5d93e3c9e3e78e1c
jakkit <jukkit-st@rmutsb.ac.th>
2019-09-23 21:04:25 +0700

    DBjukkit

---
+++ database2/Controllers/bookController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using database2.Data;
+using database2.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace database2.Controllers
+{
+    public class bookController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public bookController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var model = await _context.books.ToListAsync();
+            return View(model);
+        }
+
+        [Authorize]
+        public IActionResult addbook()
+        {
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> addbook(book model)
+        {
+            if (ModelState.IsValid)
+            {
+                var oldbook = await _context.books.AnyAsync(b => b.name == model.name);
+                if (oldbook)
+                {
+                    return View();
+
+                }
+                else
+                {
+                    _context.books.Add(model);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("index");
+                }
+
+            }
+            return View(model);
+        }
+        [Authorize]
+        public async Task<IActionResult> Bookedit(int id)
+        {
+                var book = await _context.books.FirstOrDefaultAsync(prayuth => prayuth.id == id);
+                if (book == null)
+                {
+                    return RedirectToAction("index");
+                }
+                else
+                {
+                    return View(book);
+                }
+            }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> editbook(book model)
+        {
+            if (ModelState.IsValid)
+            {
+                var book = await _context.books.FirstOrDefaultAsync(prayuth => prayuth.id == model.id);
+                if (book != null)
+                {
+                    book.name = model.name;
+                    book.price = model.price;
+                    await _context.SaveChangesAsync();
+                }
+                return RedirectToAction("index");
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Bookdelete(int id)
+        {
+            var book = await _context.books.FirstOrDefaultAsync(prayuth => prayuth.id == id);
+            if (book != null)
+            {
+                _context.books.Remove(book);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("index");
+            }
+        }
+    }
---
+++ database2/Data/Migrations/25620923080705_book.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace database2.Data.Migrations
+{
+    public partial class book : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "books",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(nullable: true),
+                    price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_books", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "books");
+        }
+    }
+}
